3b8a97d2d0ea670d62de523eb0ac2c6cf727de5b
Bianca Niemann <payrollsollutions@gmail.com>
2023-04-04 07:55:45 +0200

    prisma commit

--- app/routes/__app/expenses/add.jsx
+++ app/routes/__app/expenses/add.jsx
@@ -1,12 +1,15 @@
 import ExpenseForm from "~/components/expenses/ExpenseForm"
 import Modal from "../../../components/util/Modal"
 import { useNavigate } from "@remix-run/react"
+import { addExpense } from "../../../data/expenses.server"
+import { redirect } from "@remix-run/node"
+import { validateExpenseInput } from "../../../data/validation.server"
 
-export default function AddExpense(){
+export default function AddExpense() {
 
     const navigate = useNavigate()
 
-    function closeHandler(){
+    function closeHandler() {
         navigate('..')
     }
 
@@ -17,6 +20,17 @@ export default function AddExpense(){
     )
 }
 
-export function action() {
+export async function action({ request }) {
+    const formData = await request.formData()
+    const expenseData = Object.fromEntries(formData)
     
+    try {
+        validateExpenseInput(expenseData)
+    } catch (error){
+        return error
+    }
+   
+
+    await addExpense(expenseData)
+    return redirect('/expenses')
 }
